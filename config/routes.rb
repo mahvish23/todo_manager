e69d2a3b35c8fe5f6406b9301643e3ce86212eff
Mahvish <mahvish2306@gmail.com>
2021-04-25 03:01:05 +0530

    Add ability to create new todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "todos" => "todos#index"
-  get "todos/:id", to: "todos#show"
+  get "todos/create" => "todos#create"
   get "/", to: "home#index"
 end
