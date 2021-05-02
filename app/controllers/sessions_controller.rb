0988303e0c6293bcf8bcc092266c22315a990d3d
Mahvish <mahvish2306@gmail.com>
2021-05-03 05:05:09 +0530

    added signin,updating session, fencing off protected routs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
       render plain: "hi"
     else
       render plain: "incorrect"
