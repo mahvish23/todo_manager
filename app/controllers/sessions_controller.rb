8ef6af6268a3ecb9becf85235719090fcfe32ac4
Mahvish <mahvish2306@gmail.com>
2021-05-03 06:06:44 +0530

    Render invalid login attemp flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to "/"
     else
+      flash[:error] = "Your login attempt was invalid. Please retry!"
       redirect_to new_session_path
     end
   end
